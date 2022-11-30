f6adb38cde8f10dccd0930dfc6daaa7f8f58d99c
Max <shadow.iqka@qq.com>
2022-12-01 04:42:19 +0800

    [add] Widget list

--- scripts/demo.js
+++ scripts/demo.js
@@ -1,5 +1,5 @@
 function Data() {
-  return Process(
+  Process(
     "yao.table.Insert",
     "pet",
     ["name", "type", "status", "mode", "stay", "cost", "doctor_id"],
@@ -7,6 +7,16 @@ function Data() {
       ["Cookie", "cat", "checked", "enabled", 200, 105, 1],
       ["Baby", "dog", "checked", "enabled", 186, 24, 1],
       ["Poo", "others", "checked", "enabled", 199, 66, 1],
+    ]
+  );
+
+  Process(
+    "models.category.Insert",
+    ["name", "stock", "status", "rank"],
+    [
+      ["机器人", 100, "启用", 1],
+      ["运输车", 80, "启用", 2],
+      ["货柜", 100, "停用", 3],
     ]
   );
 }
