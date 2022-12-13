160a03ce69d0908d697f86d8af0cd3552a88b80a
Max <shadow.iqka@qq.com>
2022-12-13 23:30:19 +0800

    [add] on change list

--- scripts/event.js
+++ scripts/event.js
@@ -22,3 +22,28 @@ function OnChange(query) {
     setting: setting,
   };
 }
+
+/**
+ * Debug
+ * @param {*} query
+ */
+function OnChangeList(query) {
+  query = query || {};
+  field = query.field; // 字段名称
+  newVal = query.value; // 新数值
+  oldVal = query.old; // 原始值
+  extra = query.extra; // 开发者定义数据
+
+  let data = { stock: 1000, query: query }; // 更新消费金额数据, 消费金额
+  let setting = Process("yao.form.Setting", "pet", data); // 根据新数值生成配置信息;
+
+  // 错误检测
+  if (setting && setting.code && setting.message) {
+    throw new Exception(setting.message, 500);
+  }
+
+  return {
+    data: data, // 更新消费金额数据, 消费金额
+    setting: setting,
+  };
+}
